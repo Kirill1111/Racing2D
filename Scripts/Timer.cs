9516b557e7d1bf0f1218a9518c500930fda173ee
Kirill1111 <kirill.kozinets@mail.ru>
2018-08-31 21:38:25 +0700

    Улучшен код

--- Scripts/Timer.cs
+++ Scripts/Timer.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
@@ -24,30 +26,56 @@ public class Timer : MonoBehaviour {
 
     void StartTime()
     {
+
+        if (TimeStart == -1)
+        {
+            TimerStart.GetComponent<Text>().text = "";
+        }
+        if (TimeStart == 0)
+        {
+            TimerStart.GetComponent<Text>().text = "Start!";
+        }
         if (TimeStart > 0)
         {
             TimerStart.GetComponent<Text>().text = TimeStart.ToString();
-            TimeStart--;
         }
+        TimeStart--;
     }
 
-	void Time () {
-        TimerStart.GetComponent<Text>().text = "";
-        if (time > 0)
+    void Time () {
+        if (Car.GetComponent<Points>().isFinish == true)
         {
+            if(time>0)
+            TimerStart.GetComponent<Text>().text = "Вы победили";
+            time = -1;
+            Invoke("LoadScene", 5);
+        }
+        if (time > 0)
+        {         
+            time--;
             isStart = true;
             Timer1.GetComponent<Text>().text = time.ToString();
-            time--;
-        }else
+        }
         if (time == 0)
         {
             Timer1.GetComponent<Text>().text = "";
             isStart = true;
             if (Car.GetComponent<Points>().isFinish != true)
             {
+                TimerStart.GetComponent<Text>().text = "Вы проиграли";
                 isStart = false;
+                Invoke("LoadScene",5);
+            }
+            else
+            {
+                TimerStart.GetComponent<Text>().text = "Вы победили";
                 time = -1;
+                Invoke("LoadScene", 5);
             }
         }
+    }
+    void LoadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
