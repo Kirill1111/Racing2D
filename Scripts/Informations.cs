29b4a74dcb3dd1adf5ed942fd2e5d37c6702bd68
Kirill1111 <kirill.kozinets@mail.ru>
2018-08-31 21:43:07 +0700

    Создан клас , отвечающий за хранение глобальных переменных

---
+++ Scripts/Informations.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Informations : MonoBehaviour {
+
+    public static int CarId;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
